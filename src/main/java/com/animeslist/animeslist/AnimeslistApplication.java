fe2f748ba7513df86f63b466d61c0149815d6b17
VitaoIF <victormelo1212@gmail.com>
2026-05-16 20:10:43 -0300

    Initial commit

---
+++ src/main/java/com/animeslist/animeslist/AnimeslistApplication.java
@@ -0,0 +1,13 @@
+package com.animeslist.animeslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animeslist/animeslist/AnimeslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animeslist.animeslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
