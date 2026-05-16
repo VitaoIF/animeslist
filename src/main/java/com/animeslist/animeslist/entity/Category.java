4e336c52bf19d26ccccc9059edcf119eb2ea2a80
VitaoIF <victormelo1212@gmail.com>
2026-05-16 20:15:40 -0300

    feat: Adiciona classe category

---
+++ src/main/java/com/animeslist/animeslist/entity/Category.java
@@ -0,0 +1,24 @@
+package com.animeslist.animeslist.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_category")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+}
