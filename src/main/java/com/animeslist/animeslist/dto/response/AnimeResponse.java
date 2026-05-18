d2c42859233a828c562f288d3f612d9564c7afaf
VitaoIF <victormelo1212@gmail.com>
2026-05-18 02:48:40 -0300

    refact: adiciona formatação para data/mês/ano em response

--- src/main/java/com/animeslist/animeslist/dto/response/AnimeResponse.java
+++ src/main/java/com/animeslist/animeslist/dto/response/AnimeResponse.java
@@ -1,5 +1,6 @@
 package com.animeslist.animeslist.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
 import java.time.LocalDate;
@@ -10,7 +11,10 @@ public record AnimeResponse(
         Long id,
         String tittle,
         String description,
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
         LocalDate releaseDate,
+
         double rating,
         List<CategoryResponse> categoryResponseList,
         List<StreamingResponse> streamingResponses
