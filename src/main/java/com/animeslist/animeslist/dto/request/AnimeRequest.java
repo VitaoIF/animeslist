9d244e85114ee5ac8784111b8cc5e63179355d06
VitaoIF <victormelo1212@gmail.com>
2026-05-18 02:48:57 -0300

    refact: adiciona formatação para data/mês/ano na request

--- src/main/java/com/animeslist/animeslist/dto/request/AnimeRequest.java
+++ src/main/java/com/animeslist/animeslist/dto/request/AnimeRequest.java
@@ -1,11 +1,16 @@
 package com.animeslist.animeslist.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDate;
 import java.util.List;
 
 public record AnimeRequest(String tittle,
                            String description,
+
+                           @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
                            LocalDate releaseDate,
+
                            double raiting,
                            List<Long> categories,
                            List<Long> streamings) {
