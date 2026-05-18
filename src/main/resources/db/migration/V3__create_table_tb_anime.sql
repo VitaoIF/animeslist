a41e1b387c466f0a516a629f4b036f65a732ae3d
VitaoIF <victormelo1212@gmail.com>
2026-05-18 02:26:17 -0300

    feat: adiciona migration para tabela anime

---
+++ src/main/resources/db/migration/V3__create_table_tb_anime.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tb_anime(
+    id serial PRIMARY KEY,
+    tittle varchar(255) NOT NULL,
+    description text,
+    release_date date,
+    rating numeric,
+    created_at timestamp,
+    updated_at timestamp
+);
