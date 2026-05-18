4c5dfb357708f4dedc83d658667a9b649e3dabc5
VitaoIF <victormelo1212@gmail.com>
2026-05-18 02:47:51 -0300

    refact: adiciona metodos para corrigir null em categoria e streaming

--- src/main/java/com/animeslist/animeslist/service/AnimeService.java
+++ src/main/java/com/animeslist/animeslist/service/AnimeService.java
@@ -1,9 +1,13 @@
 package com.animeslist.animeslist.service;
 
 import com.animeslist.animeslist.entity.Anime;
+import com.animeslist.animeslist.entity.Category;
+import com.animeslist.animeslist.entity.Streaming;
 import com.animeslist.animeslist.repository.AnimeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,11 +16,37 @@ public class AnimeService {
     @Autowired
     private AnimeRepository animeRepository;
 
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private StreamingService streamingService;
+
     public Anime insert(Anime anime){
+        anime.setCategories(this.findCategories(anime.getCategories()));
+        anime.setStreamings(this.findStreamings(anime.getStreamings()));
         return animeRepository.save(anime);
     }
 
     public List<Anime> findAll() {
         return animeRepository.findAll();
+    }
+
+    private List<Category> findCategories(List<Category> categories){
+        List<Category> categoryList = new ArrayList<>();
+        for (Category category: categories){
+            categoryService.findById(category.getId()).ifPresent(categoryList::add);
+        }
+
+        return categoryList;
+    }
+
+    private List<Streaming> findStreamings(List<Streaming> streamings){
+        List<Streaming> streamingsList = new ArrayList<>();
+        for (Streaming streaming: streamings){
+            streamingService.findById(streaming.getId()).ifPresent(streamingsList::add);
+        }
+
+        return streamingsList;
     }
 }
