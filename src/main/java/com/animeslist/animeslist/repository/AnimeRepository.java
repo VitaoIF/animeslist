746e8696e15c4c1dbc2b295d647688e339fd7b55
VitaoIF <victormelo1212@gmail.com>
2026-05-18 03:49:21 -0300

    refact: adiciona novo metodo para encontrar anime por categorias

--- src/main/java/com/animeslist/animeslist/repository/AnimeRepository.java
+++ src/main/java/com/animeslist/animeslist/repository/AnimeRepository.java
@@ -1,7 +1,12 @@
 package com.animeslist.animeslist.repository;
 
 import com.animeslist.animeslist.entity.Anime;
+import com.animeslist.animeslist.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AnimeRepository extends JpaRepository<Anime, Long> {
+
+    List<Anime> findAnimeByCategories(List<Category> categories);
 }
